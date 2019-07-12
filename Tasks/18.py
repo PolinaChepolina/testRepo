ac4963dbd524edae224b94d0d811ee4b649a51ea
PolinaChepolina <47140698+PolinaChepolina@users.noreply.github.com>
2019-07-13 00:44:47 +0300

    Update 18.py

--- Tasks/18.py
+++ Tasks/18.py
@@ -1,9 +1,4 @@
-Python 3.7.4 (v3.7.4:e09359112e, Jul  8 2019, 14:36:03) 
-[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-=============================== RESTART: Shell ===============================
->>> def fact(n):
+ def fact(n):
     if n == 0:
         return 1
     return fact(n-1) * n
