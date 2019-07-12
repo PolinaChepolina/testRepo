c43d1f87e168fc18b2257aba74eca563ebbadf95
PolinaChepolina <47140698+PolinaChepolina@users.noreply.github.com>
2019-07-13 00:47:15 +0300

    Update 6.py

--- Tasks/6.py
+++ Tasks/6.py
@@ -1,7 +1,4 @@
-Python 3.7.4 (v3.7.4:e09359112e, Jul  8 2019, 14:36:03) 
-[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> def sgn(x):
+def sgn(x):
     if x > 0:
         return '+'
     else:
