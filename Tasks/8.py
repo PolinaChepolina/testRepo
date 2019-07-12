65dc8711a34824586b2c1572373f2ffcf8fdd948
PolinaChepolina <47140698+PolinaChepolina@users.noreply.github.com>
2019-07-13 00:45:55 +0300

    Update 8.py

--- Tasks/8.py
+++ Tasks/8.py
@@ -1,7 +1,4 @@
-Python 3.7.4 (v3.7.4:e09359112e, Jul  8 2019, 14:36:03) 
-[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> a,b,c = map(int, input('a,b,c')).split())
+ a,b,c = map(int, input('a,b,c')).split())
 if a== b==c:
     print(3)
 elif a==b or a==c or b==c:
